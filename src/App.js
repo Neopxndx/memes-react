79070e0b39fcbbd9cb433a2725e6acf7821c6718
Neopxndx <64292447+Neopxndx@users.noreply.github.com>
2022-01-05 16:22:11 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import html2canvas from 'html2canvas';
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  const onChangeLinea1 = function(evento){
+    setLinea1(evento.target.value)
+  }
+
+  const onChangeLinea2 = function(evento){
+    setLinea2(evento.target.value)
+  }
+
+  const onChangeImagen = function(evento){
+    setImagen(evento.target.value)
+  }
+
+  const onClickExportar = function(evento)  {
+    html2canvas(document.querySelector("#meme")).then(canvas =>{
+      var img = canvas.toDataURL("image/png");
+      var link = document.createElement('a');
+      link.download = 'meme.png';
+      link.href = img;
+      link.click();
+    });
+  }
+
+  return (
+    <div className="App">
+
+      <select onChange={onChangeImagen}>
+        <option value="1">Spiderman1</option>
+        <option value="2">Spiderman2</option>
+        <option value="3">Spiderman3</option>
+        <option value="4">Spiderman4</option>
+      </select> <br />
+
+      <input onChange={onChangeLinea1} type="text" placeholder='Pon la primer linea'/><br />
+      <input onChange={onChangeLinea2} type="text" placeholder='Pon la segunda linea'/><br />
+
+      <button onClick={onClickExportar} >Exportar</button>
+
+      <div className="meme" id="meme">
+        <span>{linea1}</span> <br />
+        <span>{linea2}</span> <br />
+        <img src={"/img/" + imagen + ".jpg"} />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
